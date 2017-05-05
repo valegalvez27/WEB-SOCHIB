7a05d6005dbf1a8d169593731c34ed1df411a99d
Diego Agulló <Diego Agull>
2017-05-04 23:08:03 -0300

    mas cambios de angular

--- blog-slot.component.js
+++ blog-slot.component.js
@@ -1,38 +1,58 @@
 angular.
 	module('sochib').
 		component('blogSlot', {
-			template:'<div class="row">'+
+			template:'<div class="row" ng-repeat="noti in $ctrl.noticias">'+
                 '<div class="span1">'+
                 '</div>'+
                 '<div class="span8">'+
-                  '<a href="blog-detail.htm"><img src="img/blog/1.jpg" alt=""></a>'+
+                  '<a href="blog-detail.htm"><img src="img/blog/{{noti.imagen}}" alt=""></a>'+
                   '<div class="row">'+
                     '<div class="span8 post-d-info">'+
-                      '<a href="blog-detail.htm"><h3>Ancient Timbuktu Texts in Danger?</h3></a>'+
+                      '<a href="blog-detail.htm"><h3>{{noti.titulo}}</h3></a>'+
                       '<div class="blue-dark">'+
-                      '<i class="icon-user"></i> By Admin'+
+                      '<i class="icon-user"></i> By {{noti.autor}}'+
                       '</div>'+
                       '<p>'+
-                      'Donec sed odio dui. Nulla vitae elit libero, a pharetra augue. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.Pie wafer wypas candy canes toffee. Cookie icing candy jelly oat cake chupa chups bear claw.'+
+                      '{{noti.summary}}.'+
                       '</p>'+
                     '</div>'+
                   '</div>'+
                 '</div>'+
               '</div>',
             controller: function BlogSlotController($http,$location,$q) {
-            	function getCantidad() {
-            		var pag = $location.search().page;
-            		$http.get('/data/noticias.json').then(function(response) {
-            			return ( response.data.ultima - (page * 3) );
-            		});
-            	}
 
             	function getNoticia(id) {
-            		$http.get('/data/noticias/'+id+'.json').then(function(response){
-            			return response.data;
-            		});
+            		return $http.get('/data/noticias/'+id+'.json');
             	}
 
-            	
+            	var notis = [];
+        		var pag = $location.search().page;
+        		var n = 1;
+        		$http.get('/data/noticias.json').then(function(response) {
+        			var max = response.data.ultima;
+        			n = max - ( (pag - 1) * 3);
+        		});
+
+				console.log(n);
+
+            	for(i=0;i<3;i++) {
+            		var id = n-i;
+            		if(id<1)
+            			break;
+            		notis.push(getNoticia(id));
+            	}
+
+            	self.noticias = [];
+
+            	$q.all(notis).then(function(response) {
+            		for(i=0;i<response.length;i++) {
+			          var k = n-i;
+			          var dataF = $.extend(response[i].data[0],{'id':k});
+			          console.log(dataF);
+			          self.noticias.push(dataF);
+			        }
+        
+            	});
+           	 	
             }
 		});
